a18485d5e5ecbcea217f5a513cd37cadd96be840
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-28 19:44:07 -0400

    Configure sign up page for sign up Configure SignupPage props to recieve router props Connect SignupPage to store

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,53 +1,71 @@
-import React from 'react'
-import { Form, Input, Divider, Typography, Button } from 'antd'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Form, Input, Divider, Typography, Button, message } from 'antd'
 import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, RouteComponentProps } from 'react-router-dom'
+import { signUp } from '../actions/userActions'
 import RegistrationWrapper from '../components/RegistrationWrapper' 
+import { UserState, UserThunkDispatch } from '../types/userTypes';
 
 const { Title, Text, Link } = Typography
 
-interface SigninFormValues {
+export interface SignupValues {
   display_name: string
   username: string
   password: string
 }
 
-const SigninPage: React.FC = () => {
-  const [form] = Form.useForm()
+interface Props extends RouteComponentProps {
+  signUp: (values: SignupValues) => Promise<any>
+  isLoggedIn: boolean
+}
 
-  const onFinish = (values: SigninFormValues): void => {
-    console.log(values)
+const SigninPage = (props: Props): JSX.Element => {
+  useEffect(() => {
+    if (props.isLoggedIn) {
+      props.history.replace('/dashboard')
+    }
+  }, [props.isLoggedIn, props.history])
+
+
+  const onFinish = (values: SignupValues): void => {
+    props.signUp(values)
+    .then(data => {
+      if (data.errors) {
+        message.error('There was an error submitting your request, please try again', 3)
+      }
+    })
   }
 
   return (
     <RegistrationWrapper>
       <Title>Sign Up</Title>
       <Divider />
-      <Form form={form} name="signin" colon onFinish={onFinish}>
+      <Form name="signin" colon onFinish={onFinish}>
         <Form.Item
           label="Display Name"
           name="display_name"
-          rules={[{required: true}]}
+          rules={[{required: true, message: "'Display Name' is required"}]}
         >
-          <Input prefix={<UserOutlined />} type="email" />
+          <Input prefix={<UserOutlined />} type="text" />
         </Form.Item>
         <Form.Item
           label="Email"
           name="email"
-          rules={[{required: true}]}
+          rules={[{required: true, message: "'Email' is required"}]}
         >
           <Input prefix={<MailOutlined />} type="email" />
         </Form.Item>
         <Form.Item
           label="Password"
           name="password"
-          rules={[{required: true}]}
+          rules={[{required: true, message: "'Password' is required"}]}
         >
           <Input prefix={<LockOutlined />} type="password" />
         </Form.Item>
         <Form.Item>
           <Button block type="primary" htmlType="submit">
-            Signup
+            Register
           </Button>
         </Form.Item>
       </Form>
@@ -57,4 +75,16 @@ const SigninPage: React.FC = () => {
   )
 }
 
-export default SigninPage
+const mapStateToProps = (state: UserState) => {
+  return {
+    isLoggedIn: state.isLoggedIn
+  }
+}
+
+const mapDispatchToProps = (dispatch: UserThunkDispatch) => {
+  return {
+    signUp: (values: SignupValues) => dispatch(signUp(values))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SigninPage)
