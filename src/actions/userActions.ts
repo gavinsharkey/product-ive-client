e4d18ceb1b440ace2aa64b41c10ef9376ed948d2
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-28 19:42:06 -0400

    Add errorLoadingUser action creator Add signUp action Add login action configure checkLoggedInStatus action

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -1,4 +1,6 @@
-import { User, UserActionTypes, UserThunk } from '../types/userTypes'
+import { User, UserError, UserActionTypes, UserThunk } from '../types/userTypes'
+import { LoginValues } from '../pages/LoginPage'
+import { SignupValues } from '../pages/SignupPage'
 import { fetchWithAuth } from '../concerns/fetchWithAuth'
 
 
@@ -10,15 +12,50 @@ export const loadingUser = (): UserActionTypes => {
   return {type: 'LOADING_USER'}
 }
 
-export const checkLoggedInStatus = (): UserThunk => {
+export const errorLoadingUser = (errors: UserError): UserActionTypes => {
+  return { type: 'ERROR_LOADING_USER', payload: errors }
+}
+
+export const checkLoggedInStatus = (): UserThunk<void> => {
   return dispatch => {
     dispatch(loadingUser())
     fetchWithAuth('http://localhost:3001/logged_in', 'GET')
     .then(data => {
-      console.log(data)
       if (!data.errors) {
         dispatch(setUser(data))
       }
+    })
+  }
+}
+
+export const login = (values: LoginValues): UserThunk<Promise<any>> => {
+  return async dispatch => {
+    return fetchWithAuth('http://localhost:3001/login', 'POST', {
+      ...values
+    })
+    .then(data => {
+      if (!data.error) {
+        dispatch(setUser(data.user))
+        localStorage.setItem('_product_ive_token', data.jwt)
+      }
+      return data
+    })
+  }
+}
+
+export const signUp = (values: SignupValues): UserThunk<Promise<any>> => {
+  return async dispatch => {
+    return fetchWithAuth('http://localhost:3001/signup', 'POST', {
+      user: {
+        ...values
+      }
+    })
+    .then(data => {
+      if (!data.errors) {
+        dispatch(setUser(data.user))
+        localStorage.setItem('_product_ive_token', data.jwt)
+      }
+      return data
     })
   }
 }
