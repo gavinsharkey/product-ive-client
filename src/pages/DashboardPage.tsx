9703463c322090eceb03c67c23beda520b2a7219
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-01 13:04:53 -0400

    configure logic for logging out

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Route, Switch, RouteComponentProps, Redirect } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Layout } from 'antd'
 import { UserState } from '../types/userTypes'
+import { logout } from '../actions/userActions'
 import DashboardHeader from '../components/dashboard/header/DashboardHeader'
 import NotesContent from '../components/dashboard/content/NotesContent'
 import TodosContent from '../components/dashboard/content/TodosContent'
 import DefaultContent from '../components/dashboard/content/DefaultContent'
-import { useSelector } from 'react-redux'
 
 const { Footer } = Layout
 
 const DashboardPage: React.FC<RouteComponentProps> = (props) => {
   const path = props.match.path
   const id = useSelector((state: UserState) => state.user.id)
+  const dispatch = useDispatch()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    props.history.push('/')
+  }
 
   const handleLink = (route: string): void => {
     props.history.push(`${path}${route}`)
@@ -21,7 +28,7 @@ const DashboardPage: React.FC<RouteComponentProps> = (props) => {
   return (
     <>
       <Layout style={{height: '100%'}}>
-        <DashboardHeader handleLink={handleLink} />
+        <DashboardHeader handleLogout={handleLogout} handleLink={handleLink} />
         <Layout>
           <Switch>
             <Route exact path={path}>
