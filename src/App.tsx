4e737143b06b60aad6cc29fe36519711412729ef
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-01 10:23:41 -0400

    protect /dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { useDispatch } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import { checkLoggedInStatus } from './actions/userActions'
+import ProtectedRoute from './components/router/ProtectedRoute'
 import LoginScreen from './pages/LoginPage'
 import WelcomePage from './pages/WelcomePage';
 import SignupPage from './pages/SignupPage'
@@ -19,7 +20,7 @@ const App: React.FC = () => {
       <Route exact path="/" component={WelcomePage} />
       <Route exact path="/login" component={LoginScreen} />
       <Route exact path="/signup" component={SignupPage} />
-      <Route exact path="/dashboard" component={DashboardPage} />
+      <ProtectedRoute exact path="/dashboard" component={DashboardPage} />
     </Switch>
   );
 }
