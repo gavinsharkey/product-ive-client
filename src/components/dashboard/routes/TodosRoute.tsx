907316e4d87540c71b3903f1c166068242a54db3
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-06 13:45:44 -0400

    Add themed sider and basic styling to todos route

--- src/components/dashboard/routes/TodosRoute.tsx
+++ src/components/dashboard/routes/TodosRoute.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import ThemedSider from '../../ThemedSider'
 
 const { Sider, Content } = Layout
 
 const TodosRoute: React.FC = (props) => {
   return (
     <>
-      <Sider className="sider-navigation" collapsible></Sider>
+      <ThemedSider>
+        <Menu
+            onSelect={values => console.log(values)}
+            mode="inline"
+            theme="light"
+          >
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              All Todos
+            </Menu.Item>
+          </Menu>
+      </ThemedSider>
       <Content>Todos</Content>
     </>
   )
