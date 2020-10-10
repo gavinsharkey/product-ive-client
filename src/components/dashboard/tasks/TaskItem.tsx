f76e881f6b5261881ddf36f1f0059964b2d081fc
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 15:23:56 -0400

    Add functionality for editing name Add functionality for toggling completed

--- src/components/dashboard/tasks/TaskItem.tsx
+++ src/components/dashboard/tasks/TaskItem.tsx
@@ -6,19 +6,37 @@ import './TaskItem.css'
 
 interface TaskItemProps {
   task: Task
+  handleSetCompleted: (id: number, completed: boolean) => void
+  handleEditName: (id: number, value: string) => void
 }
 
-const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
+const TaskItem: React.FC<TaskItemProps> = ({ task, handleSetCompleted, handleEditName }) => {
 
   const CompletedStatusIcon = task.completed ? CheckCircleOutlined : CloseCircleOutlined
+  const completedStyle = { background: task.completed ? '#1DA57A' : '#ddd' }
 
   return (
     <div className="task-item">
+      <div
+        className="task-item-completed"
+        style={completedStyle}
+        onClick={() => handleSetCompleted(task.id, task.completed)}
+      >
+        <CompletedStatusIcon />
+      </div>
       <div className="task-item-content">
-        <div className="task-item-completed">
-          <CompletedStatusIcon />
-        </div>
-        <Typography.Text>{task.name}</Typography.Text>
+        <Typography.Text 
+          editable={{
+            autoSize: { maxRows: 2 },
+            onChange: (value) => {
+              if (value.length > 0) {
+                handleEditName(task.id, value)
+              }
+            } 
+          }}
+        >
+          {task.name}
+        </Typography.Text>
       </div>
       <div className="task-item-data">
         <Typography.Text>{task.taskable.name}</Typography.Text>
