070f848cb58b3254ccd5a23c860f5bb4828611d3
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 18:17:30 -0400

    Add taskable_type to Task type

--- src/types/tasksTypes.ts
+++ src/types/tasksTypes.ts
@@ -3,6 +3,7 @@ import { User } from './userTypes';
 
 export interface Task {
   id: number
+  taskable_type: 'TaskGroup' | 'User'
   name: string
   completed: boolean
   taskable: TaskGroup & User
