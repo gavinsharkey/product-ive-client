9e39b426cc6b2a23ce00bbaebad363936efdc0b9
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 10:16:52 -0400

    Configure task fetching for task container

--- src/components/dashboard/tasks/TasksContainer.tsx
+++ src/components/dashboard/tasks/TasksContainer.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Typography, Divider } from 'antd'
+import ThemedSkeleton from '../../ThemedSkeleton'
+import TaskItem from './TaskItem'
 import './TasksContainer.css'
+import { Task } from '../../../types/tasksTypes'
+import { fetchWithAuth } from '../../../concerns/fetchWithAuth'
 
 const { Title } = Typography
 
@@ -9,10 +13,43 @@ interface TasksContainerProps {
 }
 
 const TasksContainer: React.FC<TasksContainerProps> = (props) => {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true)
+      let data
+
+      if (props.taskKey === 'all') {
+        data = await fetchWithAuth(`http://localhost:3001/tasks`)
+      } else {
+        data = await fetchWithAuth(`http://localhost:3001/tasks?taskable_id=${props.taskKey}`)
+      }
+
+      setTasks(data)
+      setLoading(false)
+    })()
+  }, [props.taskKey])
+
+  const renderedTasks = () => {
+    return tasks.map((task: Task) => {
+      return <TaskItem task={task} />
+    })
+  }
+
   return (
     <div className="tasks-container">
       <Title>All Tasks</Title>
       <Divider />
+      { loading
+        ? <ThemedSkeleton paragraph active />
+        : (
+          <div className="tasks">
+            {renderedTasks()}
+          </div>
+        )
+      }
     </div>
   )
 }
