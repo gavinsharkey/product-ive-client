6b95912e629eceba0396c1ec524c34f396a141a8
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-16 18:47:13 -0400

    Connect App to store render loading screen if loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react';
-import { useDispatch } from 'react-redux'
+import { Typography } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import { checkLoggedInStatus } from './actions/userActions'
+import { UserState } from './types/userTypes'
 import ProtectedRoute from './components/router/ProtectedRoute'
 import LoginScreen from './pages/LoginPage'
 import WelcomePage from './pages/WelcomePage';
@@ -9,11 +11,18 @@ import SignupPage from './pages/SignupPage'
 import DashboardPage from './pages/DashboardPage'
 
 const App: React.FC = () => {
+  const status = useSelector((state: UserState) => state.status)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(checkLoggedInStatus())
-  })
+  }, [dispatch])
+
+  if (status === 'loading') {
+    return (
+      <Typography.Title>Loading...</Typography.Title>
+    )
+  }
 
   return (
     <Switch>
