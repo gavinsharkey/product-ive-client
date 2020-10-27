2947ea58dd2e840e1fc6b90a935da28ab7da4154
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-27 13:24:15 -0400

    Add Redux Devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import userReducer from './reducers/userReducer'
 import './index.less';
 import App from './App';
 
-const store = createStore(userReducer, applyMiddleware(thunk))
+const store = createStore(userReducer, compose(applyMiddleware(thunk), (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()))
 
 ReactDOM.render(
   <Provider store={store}>
