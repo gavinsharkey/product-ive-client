baaca89d5eab82ef916fe9fe92b62a8873bbd052
gavinsharkey <gavinsharkey2016@gmail.com>
2020-11-30 17:10:36 -0600

    Configure with redux

--- src/components/dashboard/tasks/ParentTaskItem.tsx
+++ src/components/dashboard/tasks/ParentTaskItem.tsx
@@ -1,27 +1,55 @@
 import React from 'react'
-import ChildTasksContainer from './ChildTasksContainer'
-import TaskItem, { TaskItemProps } from './TaskItem'
+import SubTasksContainer from './SubTasksContainer'
+import TaskItem from './TaskItem'
+import { useDispatch } from 'react-redux'
+import {
+  updateTaskCompleted,
+  updateTaskName,
+  deleteTask
+} from '../../../actions/tasksActions'
+import { Task } from '../../../types/tasksTypes'
+
 import './ParentTaskItem.css'
 
-interface ParentTaskItemProps extends TaskItemProps {
+interface ParentTaskItemProps {
+  task: Task
   isSelected: boolean
   handleSelectTask: (id: number | null) => void
 }
 
-const ParentTaskItem: React.FC<ParentTaskItemProps> = ({isSelected, handleSelectTask, ...rest}) => {
+const ParentTaskItem: React.FC<ParentTaskItemProps> = ({isSelected, handleSelectTask, task}) => {
+  const dispatch = useDispatch()
+
   const handleSelect = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation()
 
-    isSelected ? handleSelectTask(null) : handleSelectTask(rest.task.id)
+    isSelected ? handleSelectTask(null) : handleSelectTask(task.id)
+  }
+
+  const handleUpdateCompleted = () => {
+    dispatch(updateTaskCompleted(task.id))
+  }
+
+  const handleUpdateName = (name: string) => {
+    dispatch(updateTaskName(task.id, name))
+  }
+
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id))
   }
 
   return (
     <div className="parent-task-wrapper">
       <div onClick={handleSelect} className={`parent-task-item task-item ${isSelected ? 'selected' : ''}`}>
-        <TaskItem {...rest} />
+        <TaskItem
+          task={task}
+          handleUpdateCompleted={handleUpdateCompleted}
+          handleUpdateName={handleUpdateName}
+          handleDelete={handleDelete}
+        />
       </div>
       { isSelected
-        ? <ChildTasksContainer parentTaskId={rest.task.id} />
+        ? <SubTasksContainer parentTaskId={task.id} />
         : null
       }
     </div >
